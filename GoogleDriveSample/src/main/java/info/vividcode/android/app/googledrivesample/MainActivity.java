18966417a25600b8bdddbb4658b163d1e19b0631
nobuoka <nobuoka@vividcode.info>
2013-09-08 12:29:49 +0900

    Use Google Drive

--- GoogleDriveSample/src/main/java/info/vividcode/android/app/googledrivesample/MainActivity.java
+++ GoogleDriveSample/src/main/java/info/vividcode/android/app/googledrivesample/MainActivity.java
@@ -1,10 +1,41 @@
 package info.vividcode.android.app.googledrivesample;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
+import android.accounts.AccountManager;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.ByteArrayContent;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 
 public class MainActivity extends Activity {
+    static final int REQUEST_ACCOUNT_PICKER = 1;
+    static final int REQUEST_AUTHORIZATION = 2;
+
+    static final String FILE_TITLE = "google_drive_test";
+
+    private Drive service = null;
+    private GoogleAccountCredential credential = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,12 +43,166 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (service == null) {
+            credential = GoogleAccountCredential.usingOAuth2(this, Arrays.asList(DriveScopes.DRIVE));
+            startActivityForResult(credential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
+        }
+
+        ((Button)findViewById(R.id.saveButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("save");
+                saveTextToDrive();
+            }
+        });
+        ((Button)findViewById(R.id.loadButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("load");
+                loadTextFromDrive();
+            }
+        });
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        switch (requestCode) {
+            case REQUEST_ACCOUNT_PICKER:
+                if (resultCode == RESULT_OK && data != null && data.getExtras() != null) {
+                    String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        credential.setSelectedAccountName(accountName);
+                        service = getDriveService(credential);
+                    }
+                }
+                break;
+            case REQUEST_AUTHORIZATION:
+                if (resultCode == Activity.RESULT_OK) {
+                    saveTextToDrive();
+                } else {
+                    startActivityForResult(credential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
+                }
+                break;
+        }
     }
-    
+
+    private void saveTextToDrive() {
+        final String inputText = ((EditText)findViewById(R.id.editText)).getText().toString();
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // 指定のタイトルのファイルの ID を取得
+                    String fileIdOrNull = null;
+                    FileList list = service.files().list().execute();
+                    for (File f : list.getItems()) {
+                        if (FILE_TITLE.equals(f.getTitle())) {
+                            fileIdOrNull = f.getId();
+                        }
+                    }
+
+                    File body = new File();
+                    body.setTitle(FILE_TITLE);//fileContent.getName());
+                    body.setMimeType("text/plain");
+
+                    ByteArrayContent content = new ByteArrayContent("text/plain", inputText.getBytes(Charset.forName("UTF-8")));
+                    if (fileIdOrNull == null) {
+                        service.files().insert(body, content).execute();
+                        showToast("insert!");
+                    } else {
+                        service.files().update(fileIdOrNull, body, content).execute();
+                        showToast("update!");
+                    }
+                    // TODO 失敗時の処理?
+                } catch (UserRecoverableAuthIOException e) {
+                    startActivityForResult(e.getIntent(), REQUEST_AUTHORIZATION);
+                } catch (IOException e) {
+                    showToast("error occur...");
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
+    }
+
+    private void loadTextFromDrive() {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // 指定のタイトルのファイルの ID を取得
+                    String fileIdOrNull = null;
+                    FileList list = service.files().list().execute();
+                    for (File f : list.getItems()) {
+                        if (FILE_TITLE.equals(f.getTitle())) {
+                            fileIdOrNull = f.getId();
+                        }
+                    }
+
+                    InputStream is = null;
+                    if (fileIdOrNull != null) {
+                        File f = service.files().get(fileIdOrNull).execute();
+                        is = downloadFile(service, f);
+                    }
+                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                    try {
+                        StringBuffer sb = new StringBuffer();
+                        sb.append(br.readLine());
+
+                        final String text = sb.toString();
+                        runOnUiThread(new Runnable() {
+                            @Override public void run() {
+                                ((EditText)findViewById(R.id.editText)).setText(text);
+                            }
+                        });
+                    } finally {
+                        if (br != null) br.close();
+                    }
+                    // TODO 失敗時の処理?
+                } catch (UserRecoverableAuthIOException e) {
+                    startActivityForResult(e.getIntent(), REQUEST_AUTHORIZATION);
+                } catch (IOException e) {
+                    showToast("error occur...");
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
+    }
+
+    // https://developers.google.com/drive/v2/reference/files/get より
+    private static InputStream downloadFile(Drive service, File file) {
+        if (file.getDownloadUrl() != null && file.getDownloadUrl().length() > 0) {
+            try {
+                HttpResponse resp =
+                        service.getRequestFactory().buildGetRequest(new GenericUrl(file.getDownloadUrl()))
+                                .execute();
+                return resp.getContent();
+            } catch (IOException e) {
+                // An error occurred.
+                e.printStackTrace();
+                return null;
+            }
+        } else {
+            // The file doesn't have any content stored on Drive.
+            return null;
+        }
+    }
+
+    private Drive getDriveService(GoogleAccountCredential credential) {
+        return new Drive.Builder(AndroidHttp.newCompatibleTransport(), new GsonFactory(), credential)
+                .build();
+    }
+
+    public void showToast(final String toast) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
---
+++ README.markdown
@@ -0,0 +1,45 @@
+android-GoogleDriveSample
+===================================
+
+Android アプリから Google Drive API を使用するサンプルプロジェクト。
+
+## ビルド方法
+
+### プロジェクトのクローン
+
+クローンするのに予め準備しておく必要があるものは次のとおり。
+* [Git](http://git-scm.com/)
+
+```
+# プロジェクトを git clone する
+git clone --recursive git@github.com:nobuoka/android-GoogleDriveSample.git
+
+# 移動
+cd android-GoogleDriveSample
+```
+
+### Gradle を使用してビルドする
+
+[Android の Gradle Plugin](http://tools.android.com/tech-docs/new-build-system) を使用してビルドする。
+ビルドのために予め準備しておく必要があるものは次のとおり。
+* [Java SE Development Kit](http://www.oracle.com/technetwork/java/javase/downloads/index.html) (1.6 か 1.7)
+* Android SDK Manager で次のものをインストールしておくこと (過不足があるかもしれない):
+  * [SDK with Build Tools](http://developer.android.com/sdk/index.html): Tools の Android SDK Build-tools 18.0.0 以降
+  * Extras の Google Play services
+  * Extras の Google Repository
+
+本リポジトリには Gradle ラッパーが含まれているので、Gradle をインストールしておく必要はない。
+
+```
+# ビルド
+ANDROIND_HOME=/path/to/android_sdk ./gradlew --daemon build
+```
+
+ビルド時に Android SDK Tools を使用するので、環境変数 `ANDROID_HOME` でそのパスを指定する必要がある。
+もしくは local.properties ファイルで指定してもよい。
+`gradlew` コマンドの `--daemon` オプションは必須ではないが、付けておくとデーモンが起動するので 2 回目以降の実行がはやくなって良い。
+
+### Android Studio を使用してビルドする
+
+Android Studio にインポートすることもできる。
+インポート時に使用する Gradle を選択するが、「Gradle wrapper」 を選べばよい。
